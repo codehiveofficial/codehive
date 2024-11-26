43db77f8bb4d36241dc32446b8f46f9b28c5856d
abhaydixit07 <abhaydixit227@gmail.com>
2024-11-26 18:53:16 +0530

    Add Combined button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,11 @@
 "use client";
 
-import { useRouter } from 'next/router';
-
 export default function Home() {
-  const router = useRouter();
-
-  const navigateToCombined = () => {
-    router.push('/combined');
-  };
-
   return (
     <main>
-      <button onClick={navigateToCombined} className="px-4 py-2 bg-blue-500 text-white rounded">
+      <a href="/combined" className="px-4 py-2 bg-blue-500 text-white rounded">
         Go to Combined
-      </button>
+      </a>
     </main>
   );
 }
