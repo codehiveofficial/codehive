eacd5a96b270ace8b7a23e94b7f038d48905594b
abhaydixit07 <abhaydixit227@gmail.com>
2024-11-28 01:06:29 +0530

    Landing Page components - about , hero and contact

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
+import About from "@/components/About";
+import Contact from "@/components/Contact";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main>
-      <a href="/combined" className="px-4 py-2 bg-blue-500 text-white rounded">
-        Go to Combined
-      </a>
+      <Hero />
+      <About />
+      <Contact />
     </main>
   );
 }
