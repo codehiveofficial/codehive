d8dcd3152f511382cdc5c95c6077f15847130759
Khagesh Sharma <khageshsharma24@gmail.com>
2024-11-29 02:12:42 +0530

    Added working link to contact us button in navbar and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,9 +11,9 @@ const Footer = () => {
   return (
     <footer className="w-full mt-32 py-14 bg-black border-t border-white/[0.2]">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 flex flex-col items-center justify-center ">
-        <div className="flex items-center justify-center">
-          {/* <img src="" className="h-24" alt="Logo" /> */}
-          <p className="font-spacegrotesksemibold text-white text-2xl">
+        <div onClick={() => scrollToSection("home")} className="flex items-center justify-center">
+          <img src="" className="h-24" alt="Logo" />
+          <p className="font-spacegrotesksemibold text-white text-2xl cursor-pointer">
             Codehive
           </p>
         </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -37,7 +37,7 @@ const Navbar = () => {
             type="button"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
           >
-            <a href="http://localhost:3000/combined">Get started</a>
+            <a href="/combined">Get started</a>
           </button>
           <button
             onClick={toggleMenu}
